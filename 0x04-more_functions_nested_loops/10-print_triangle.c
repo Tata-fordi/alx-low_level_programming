3711dc5bbca9ef3ea6162b8f83adcf30ed563909
Tata-fordi <tataford1122@gmail.com>
2023-03-23 14:20:47 -0700

    juu

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+  *print_triangle - printsa triangle
+  *@size: the size
+  *Return: empty
+  */
+
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+	{
+	putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y > 1; y--)
+	{
+	putchar(32);
+	}
+	for  (z = 0; z <= x; z++)
+	{
+	putchar(35);
+	}
+	putchar('\n');
+	}
+	}
+}	
