21621a5d120ed1d3ca6d182f2db383301af720d4
Tata-fordi <tataford1122@gmail.com>
2023-03-23 07:34:13 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_numbers - checks for the digits in range
+  *
+  *Return: Always 0 (success)
+  */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
